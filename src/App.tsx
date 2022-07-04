e41e9f73107d314d2f967d16384b67454c6cdd2e
Vando Dos Reis <vandodosreis2001@gmail.com>
2022-07-04 19:34:53 -0300

    Inicializando o projeto ao git

---
+++ src/App.tsx
@@ -0,0 +1,103 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import PopupMenu from "./component/PopupMenu/PopupMenu";
+import Dropdown from "./component/Dropdown/Dropdown";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const client = [
+    {
+      id: 1,
+      name: 'Cliente 1',
+      description: 'Descrição do cliente 1',
+    },
+    {
+      id: 2,
+      name: 'Cliente 2',
+      description: 'Descrição do cliente 2',
+    },
+    {
+      id: 3,
+      name: 'Cliente 3',
+      description: 'Descrição do cliente 3',
+    },
+    {
+      id: 4,
+      name: 'Cliente 4',
+      description: 'Descrição do cliente 4',
+    },
+    {
+      id: 5,
+      name: 'Cliente 5',
+      description: 'Descrição do cliente 5',
+    }
+  ]
+
+  return (
+    <div className="container">
+      {/*<header>*/}
+      {/*  <div className="logo-area">*/}
+      {/*    <img src="" className="App-logo" alt="logo" />*/}
+      {/*  </div>*/}
+
+      {/*  <nav className="navbar">*/}
+      {/*    <Dropdown menu="Sobre">*/}
+      {/*      <li><a href="#">Parceiros</a></li>*/}
+      {/*      <li><a href="#">Equipe</a></li>*/}
+      {/*      <li><a href="#">Projetos</a></li>*/}
+      {/*    </Dropdown>*/}
+
+      {/*    <Dropdown menu="Sobre">*/}
+      {/*      <li><a href="#">Parceiros</a></li>*/}
+      {/*      <li><a href="#">Equipe</a></li>*/}
+      {/*      <li><a href="#">Projetos</a></li>*/}
+      {/*    </Dropdown>*/}
+
+      {/*    <Dropdown menu="Sobre">*/}
+      {/*      <li><a href="#">Parceiros</a></li>*/}
+      {/*      <li><a href="#">Equipe</a></li>*/}
+      {/*      <li><a href="#">Projetos</a></li>*/}
+      {/*    </Dropdown>*/}
+
+      {/*    <Dropdown menu="Sobre">*/}
+      {/*      <li><a href="#">Parceiros</a></li>*/}
+      {/*      <li><a href="#">Equipe</a></li>*/}
+      {/*      <li><a href="#">Projetos</a></li>*/}
+      {/*    </Dropdown>*/}
+      {/*  </nav>*/}
+      {/*</header>*/}
+
+      <table>
+        <thead>
+        <tr>
+          <th>Nome</th>
+          <th>Descrição</th>
+        </tr>
+        </thead>
+        <tbody>
+        {client.map(client => (
+            <tr key={client.id}>
+              <td>
+                <h3>{client.name}</h3>
+                <p>{client.description}</p>
+              </td>
+
+              <td>
+                <PopupMenu>
+                  <li className="item"><a href={'#'}>Item 1</a></li>
+                  <li className="item"><a href={'#'}>Item 2</a></li>
+                  <li className="item"><a href={'#'}>Item 3</a></li>
+                  <li className="item"><a href={'#'}>Item 4</a></li>
+                </PopupMenu>
+              </td>
+            </tr>
+        ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default App
